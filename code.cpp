8487d54b22281ff23594929bf89ecdb095d0b66b
TuT <67815316+ikun-01@users.noreply.github.com>
2023-09-02 17:24:17 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -14,7 +14,7 @@ public:
 
 int calculateMinimumInitialCharge(std::string tasksString) {
     std::vector<Task> tasks;
-    
+
     // 将输入字符串按逗号分隔为任务列表
     std::stringstream ss(tasksString);
     std::string task;
@@ -24,35 +24,30 @@ int calculateMinimumInitialCharge(std::string tasksString) {
         std::string powerStr, chargeStr;
         std::getline(task_ss, powerStr, ':');
         std::getline(task_ss, chargeStr, ':');
-        
+
         // 将消耗电量和最低初始电量转换为整数，并创建任务对象
         int power = std::stoi(powerStr);
         int charge = std::stoi(chargeStr);
         Task t(power, charge);
         tasks.push_back(t);
     }
-    
+
     int remainingCharge = 0;  // 剩余电量
     int minimumInitialCharge = 0;  // 最小初始电量
-    
+
     // 遍历每个任务，计算最小初始电量
-    for (const auto& task : tasks) {
+    for (const auto &task : tasks) {
         // 如果剩余电量不足以完成当前任务，增加最小初始电量
-        if (remainingCharge < task.powerConsumption) {
-            int chargeToAdd = task.powerConsumption - remainingCharge;
+        if (remainingCharge < task.minimumInitialCharge) {
+            int chargeToAdd = task.minimumInitialCharge - remainingCharge;
             minimumInitialCharge += chargeToAdd;
             remainingCharge += chargeToAdd;
         }
-        
+
         // 完成当前任务后，减少剩余电量
         remainingCharge -= task.powerConsumption;
     }
-    
-    // 如果最低初始电量超过了手机的最大电量，则返回-1
-    if (minimumInitialCharge > 4800) {
-        return -1;
-    }
-    
+
     return minimumInitialCharge;
 }
 
